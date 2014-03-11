1db16f580f800bd245b9e3590c882af5afdd3dcc
Steven Noonan <steven@uplinklabs.net>
2014-03-11 10:03:15 -0700

    nbody.cu: eliminate dead kbhit code, fix indentation
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/nbody.cu
+++ src/nbody.cu
@@ -102,65 +102,30 @@ static const char *rgszAlgorithmNames[] = {
 
 static int kbhit(void)
 {
-  struct termios oldt, newt;
-  int ch;
-  int oldf;
- 
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
- 
-  ch = getchar();
- 
-  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-  fcntl(STDIN_FILENO, F_SETFL, oldf);
- 
-  if(ch != EOF)
-  {
-    ungetc(ch, stdin);
-    return 1;
-  }
- 
-  return 0;
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if(ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
 }
-
-#if 0
-static int kbhit(void)
-{
-  struct timeval tv;
-  fd_set read_fd;
-
-  /* Do not wait at all, not even a microsecond */
-  tv.tv_sec=0;
-  tv.tv_usec=0;
-
-  /* Must be done first to initialize read_fd */
-  FD_ZERO(&read_fd);
-
-  /* Makes select() ask if input is ready:
- *    * 0 is the file descriptor for stdin    */
-  FD_SET(0,&read_fd);
-
-  /* The first parameter is the number of the
- *    * largest file descriptor to check + 1. */
-  if(select(1, &read_fd,NULL, /*No writes*/NULL, /*No exceptions*/&tv) == -1)
-    return 0;  /* An error occured */
-
-  /*  read_fd now holds a bit map of files that are
- *     * readable. We test the entry for the standard
- *        * input (file 0). */
-  
-if(FD_ISSET(0,&read_fd))
-    /* Character pending on stdin */
-    return 1;
-
-  /* no characters were pending */
-  return 0;
-}
-#endif
 
 // we only call getch() when kbhit() has told us there
 // is a pending keystroke
