4d2b5cf4cbc56cb5c82c1887eb103dc37952bd53
Steven Noonan <steven@uplinklabs.net>
2018-02-19 01:05:54 -0800

    nbody_util: only define ASSUME if not already found
    e.g. if we're using Clang but clang masquerades as >= gcc 4.5
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/nbody_util.h
+++ src/nbody_util.h
@@ -61,7 +61,7 @@ extern "C" {
 #      define ASSUME(cond) __builtin_assume(cond)
 #    endif
 #  endif
-#  if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5))
+#  if !defined(ASSUME) && defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 5))
 #    define ASSUME(cond) do { if (!(cond)) __builtin_unreachable(); } while (0)
 #  endif
 #  if !defined(__clang__) && !defined(TARGET_DECL)
