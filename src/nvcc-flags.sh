f5b511c83c038ef2882ba02df4f64de657ac4e8d
Steven Noonan <steven@uplinklabs.net>
2019-02-24 03:09:39 -0800

    generate NVCC target flags dynamically, based on CUDA version
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

---
+++ src/nvcc-flags.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+NVCC="$1"
+
+verlte() {
+	[[ "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ]]
+}
+verlt() {
+    [ "$1" = "$2" ] && return 1 || verlte $1 $2
+}
+
+vergte() {
+	[[ "$1" = "$(echo -e "$1\n$2" | sort -V | tail -n1)" ]]
+}
+
+NVCC_VER=$("$NVCC" --version | awk '/^Cuda comp/ { print $5 }' | cut -d',' -f 1)
+
+NVCC_FLAGS=()
+
+if [ -z "$NVCC_VER" ] || verlt ${NVCC_VER} 6.0; then
+	echo "Your installed CUDA version is too old. Please run CUDA 6.0 or higher!" >&2
+	exit 1
+fi
+
+NVCC_FLAGS+=(
+	-gencode=arch=compute_50,code=\\\"sm_50,compute_50\\\"
+	-gencode=arch=compute_52,code=\\\"sm_52,compute_52\\\"
+)
+
+if vergte ${NVCC_VER} 8.0; then
+	NVCC_FLAGS+=( -gencode=arch=compute_61,code=\\\"sm_61,compute_61\\\" )
+fi
+
+if vergte ${NVCC_VER} 9.0; then
+	NVCC_FLAGS+=( -gencode=arch=compute_70,code=\\\"sm_70,compute_70\\\" )
+fi
+
+if vergte ${NVCC_VER} 10.0; then
+	NVCC_FLAGS+=( -gencode=arch=compute_75,code=\\\"sm_75,compute_75\\\" )
+fi
+
+echo ${NVCC_FLAGS[@]}
+exit 0
