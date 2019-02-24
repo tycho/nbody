9c687145e8e9f40c8cbe948036414b6617ebf528
Steven Noonan <steven@uplinklabs.net>
2019-02-24 04:30:29 -0800

    install-ubuntu: make libomp-dev optional, for CUDA 7.5 docker env
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- .travis/install-ubuntu.sh
+++ .travis/install-ubuntu.sh
@@ -7,9 +7,13 @@ apt-get update
 apt-get install -y locales
 locale-gen en_US.UTF-8
 
-PACKAGES=(build-essential pkg-config clang libomp-dev)
+PACKAGES=(build-essential pkg-config clang)
 
 apt-get install -y "${PACKAGES[@]}"
+
+# Optional: try to install libomp-dev. It doesn't exist for the CUDA 7.5 docker
+# environment (which is based on Trusty).
+apt-get install -y libomp-dev || true
 
 # Optional: try to install meson. Doesn't exist on older Ubuntu, so this could
 # be made mandatory in the future.
