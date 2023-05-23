5ebc9b7c2f64da0e1f3a30168eaec22a2ccbf1f0
Steven Noonan <steven@uplinklabs.net>
2023-05-23 11:04:30 -0700

    nbody_util: use std::linear_congruential_engine for random
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/nbody_util.cpp
+++ src/nbody_util.cpp
@@ -57,78 +57,9 @@
 #include <fcntl.h>
 #include <sys/select.h>
 #endif
+#include <random>
 
 #include "nbody_util.h"
-
-#ifdef _WIN32
-#define RANDOM_MAX 2147483647
-/*
-this code uses the same lagged fibonacci generator as the
-original bsd random implementation except for the seeding
-which was broken in the original
-*/
-
-static uint32_t init[] = {
-    0x00000000,0x5851f42d,0xc0b18ccf,0xcbb5f646,
-    0xc7033129,0x30705b04,0x20fd5db4,0x9a8b7f78,
-    0x502959d8,0xab894868,0x6c0356a7,0x88cdb7ff,
-    0xb477d43f,0x70a3a52b,0xa8e4baf1,0xfd8341fc,
-    0x8ae16fd9,0x742d2f7a,0x0d1f0796,0x76035e09,
-    0x40f7702c,0x6fa72ca5,0xaaa84157,0x58a0df74,
-    0xc74a0364,0xae533cc4,0x04185faf,0x6de3b115,
-    0x0cab8628,0xf043bfa4,0x398150e9,0x37521657
-};
-
-static int n = 31;
-static int i = 3;
-static int j = 0;
-static uint32_t *x = init+1;
-static volatile int lock[2];
-
-static uint32_t lcg31(uint32_t x) {
-    return (1103515245*x + 12345) & 0x7fffffff;
-}
-
-static uint64_t lcg64(uint64_t x) {
-    return 6364136223846793005ull*x + 1;
-}
-
-static void srandom(unsigned seed) {
-    int k;
-    uint64_t s = seed;
-
-    if (n == 0) {
-        x[0] = s;
-        return;
-    }
-    i = n == 31 || n == 7 ? 3 : 1;
-    j = 0;
-    for (k = 0; k < n; k++) {
-        s = lcg64(s);
-        x[k] = s>>32;
-    }
-    /* make sure x contains at least one odd number */
-    x[0] |= 1;
-}
-
-static long random(void) {
-    long k;
-
-    if (n == 0) {
-        k = x[0] = lcg31(x[0]);
-        return k;
-    }
-    x[i] += x[j];
-    k = x[i]>>1;
-    if (++i == n)
-        i = 0;
-    if (++j == n)
-        j = 0;
-    return k;
-}
-#else
-#define RANDOM_MAX RAND_MAX
-#endif
 
 int processorCount(void)
 {
@@ -147,31 +78,19 @@ int processorCount(void)
 #endif
 }
 
+static std::linear_congruential_engine<std::uint32_t, 214013, 2531011, 1U << 31U> randNative;
+
 void seedRandom(unsigned int seed)
 {
-#if _XOPEN_SOURCE >= 500 || _DEFAULT_SOURCE || _SVID_SOURCE || _BSD_SOURCE
-    srandom(seed);
-#else
-    srand(seed);
-#endif
+    randNative.seed(seed);
 }
 
 float nbodyRandom(float randMin, float randMax)
 {
     float result;
     uint32_t v;
-#if defined(HIGH_ENTROPY) && defined __RDRND__
-    int i = _rdrand32_step(&v);
-    if (!i)
-        abort();
-#else
-#if _XOPEN_SOURCE >= 500 || _DEFAULT_SOURCE || _SVID_SOURCE || _BSD_SOURCE
-    v = random();
-#else
-    v = rand();
-#endif
-#endif
-    result = (float)v / (float)RANDOM_MAX;
+    v = randNative();
+    result = (float)v / (float)randNative.max();
     return ((1.0f - result) * randMin + result * randMax);
 }
 
